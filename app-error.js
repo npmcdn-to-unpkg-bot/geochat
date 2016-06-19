4817a563ded7c5b1d08e0b91712e46559507a8f6
Rakesh Gupta <grakesh18@gmail.com>
2016-06-19 14:44:54 +0530

    code refactor

---
+++ app-error.js
@@ -0,0 +1,5 @@
+var AppError = {};
+
+AppError.mongo_duplicate_column_error = "11000";
+
+module.exports = AppError;
---
+++ config-text.js
@@ -0,0 +1,6 @@
+var ConfigText = {};
+
+ConfigText.email_exists = "Email already exists";
+ConfigText.contact_administrator = "Error! Please contact administrator";
+
+module.exports = ConfigText;
