2ab5b26ecdab776be2aa3e7fcbbfe105cffd4bc1
Rakesh Gupta <grakesh18@gmail.com>
2016-06-26 00:41:51 +0530

    geolocation, login, encryption added/modified

--- util.js
+++ util.js
@@ -1,11 +1,39 @@
 "use strict";
 
 var uuid = require('node-uuid');
+var crypto = require('crypto');
+var NodeGeocoder = require('node-geocoder');
+
+var secret = "I@Love@NNNode.JS";
+
+var options = {
+  provider: 'google',
+
+  httpAdapter: 'https',
+  apiKey: 'YOUR_API_KEY',
+  formatter: null
+};
 
 var Util = function () {
   return {
             GenerateAuthToken: function () {
               return uuid.v4();
+            },
+            Encrypt: function (strToEncrypt) {
+              return crypto.createHmac('sha256', secret)
+                   .update(strToEncrypt)
+                   .digest('hex');
+            },
+            getLatLon: function (address, callback) {
+              var geocoder = NodeGeocoder(options);
+              geocoder.geocode(address, function(err, res) {
+                if (err) {
+                  callback(err.message, null)
+                }
+                else {
+                  callback(null, {"lat": res[0].latitude, "lon": res[0].longitude});
+                }
+              });
             }
   };
 }();
