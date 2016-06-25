9a7196e539baf9c403d25738ba24c8c01c006733
Rakesh Gupta <grakesh18@gmail.com>
2016-06-26 00:39:42 +0530

    geolocation, login, encryption added/modified

--- app-error.js
+++ app-error.js
@@ -3,5 +3,8 @@
 var AppError = {};
 
 AppError.mongo_duplicate_column_error = "11000";
+AppError.invalid_email_format = "Invalid email format";
+AppError.invalid_inputs_provided = "Invalid inputs provided";
+AppError.invalid_credentials = "Invalid credentials";
 
 module.exports = AppError;
